22be1a1ae5c455d000f5ba6cf54f7fefdc26b8b8
NSANTANAQ <nestorsantana12@gmail.com>
2024-08-24 17:01:42 -0500

    Probando primera pagina

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm,LoginForm
 from django.contrib.auth import views as auth_views
-
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
