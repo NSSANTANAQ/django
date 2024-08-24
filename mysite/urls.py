48ee6c97f3661891dc89c2b97156df6c1c5ab25e
NSANTANAQ <nestorsantana12@gmail.com>
2024-08-24 16:46:15 -0500

    Probando primera pagina

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', ),
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # Incluye las URLs de la app accounts
 ]
