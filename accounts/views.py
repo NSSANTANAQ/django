c10711496253854eb2501c3844fddd88e1e958a5
NSANTANAQ <nestorsantana12@gmail.com>
2024-08-24 18:38:58 -0500

    Probando primera pagina

--- accounts/views.py
+++ accounts/views.py
@@ -24,14 +24,8 @@ def signup_view(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')  # Redirige a la página principal o al lugar que prefieras
-            else:
-                form.add_error(None, 'Invalid username or password.')
+            return redirect('login')  # Redirige a la página principal o al lugar que prefieras
+
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
