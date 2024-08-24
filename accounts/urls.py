6ecc6423c1dd8773be7730c02182d90c6fac5f30
NSANTANAQ <nestorsantana12@gmail.com>
2024-08-24 16:26:52 -0500

    Probando primera pagina

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),  # Esto vincula la URL /login/ a la vista login_view
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm,LoginForm
+from django.contrib.auth import views as auth_views
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirige a la página de inicio después de registrarse
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
 def login_view(request):
     return render(request, 'accounts/login.html')  # Asegúrate de tener el template en esta ruta
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = LoginForm
