d3c861afdbdbbf9d1a4f0c3545f003ee68b7d00c
NSANTANAQ <nestorsantana12@gmail.com>
2024-08-24 18:02:05 -0500

    Probando primera pagina

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, label='Username')
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get("username")
+        password = cleaned_data.get("password")
+
+        # Aquí podrías agregar validaciones adicionales si es necesario
+        if not username or not password:
+            raise forms.ValidationError("Username and password are required.")
 
 class SignUpForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
