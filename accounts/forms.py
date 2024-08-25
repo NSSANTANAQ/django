c451750aaa14e40f728a298fde876e663c15f1ec
NSANTANAQ <nestorsantana12@gmail.com>
2024-08-24 22:03:21 -0500

    Probando primera pagina

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,29 +8,10 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, label='Username')
     password = forms.CharField(widget=forms.PasswordInput, label='Password')
 
-    def clean(self):
-        cleaned_data = super().clean()
-        username = cleaned_data.get("username")
-        password = cleaned_data.get("password")
 
-        # Aquí podrías agregar validaciones adicionales si es necesario
-        if not username or not password:
-            raise forms.ValidationError("Username and password are required.")
-
-class SignUpForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_confirmation = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        password_confirmation = cleaned_data.get('password_confirmation')
-
-        if password != password_confirmation:
-            raise forms.ValidationError("Passwords do not match")
-
-        return cleaned_data
+        fields = ('username', 'email', 'password1', 'password2')
