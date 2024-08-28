70a471a4e6d7eae799d12b53714fb1e58b819e2c
NSANTANAQ <nestorsantana12@gmail.com>
2024-08-28 08:26:52 -0500

    Probando primera pagina

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,10 @@ from django.contrib.auth import authenticate
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'autocomplete': 'username'})
+    )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'})
     )
