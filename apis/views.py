0d7aff9a7a75678da029b3ecad86ccd3dbb957fd
NSANTANAQ <nestorsantana12@gmail.com>
2024-12-11 13:08:02 -0500

    Implementación de apis

--- apis/views.py
+++ apis/views.py
@@ -3,9 +3,39 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializer import UserSerializer
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 class UserListCreateAPIView(APIView):
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+@api_view(['POST'])
+def login_view(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'success': True,
+                'message': 'Login exitoso',
+                'token': token.key
+            })
+        else:
+            return Response({
+                'success': False,
+                'message': 'Usuario o contraseña incorrectos'
+            })
+    else:
+        return Response({
+            'success': False,
+            'message': 'Método no permitido'
+        })
