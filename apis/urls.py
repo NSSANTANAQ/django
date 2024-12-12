ac433252476338a24728597bc3e5eb02be1795e2
NSANTANAQ <nestorsantana12@gmail.com>
2024-12-11 22:20:03 -0500

    Implementación de apis

--- apis/urls.py
+++ apis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import UserListCreateAPIView
+from .views import UserListCreateAPIView,login_view
 
 urlpatterns = [
     # Define tus endpoints aquí
     path('users/', UserListCreateAPIView.as_view(), name='user-list-create'),
+    path('login/', login_view, name='login'),
 ]
