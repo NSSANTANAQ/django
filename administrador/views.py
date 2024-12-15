22e2718af81a9b71f60a723891fd116d4b711b03
NSANTANAQ <nestorsantana12@gmail.com>
2024-12-14 22:19:32 -0500

    Implementación de apis

--- administrador/views.py
+++ administrador/views.py
@@ -91,7 +91,7 @@ def send_push_notification(subscription, payload):
                 },
             },
             data=json.dumps(payload),  # Convertimos a JSON
-            vapid_private_key=settings.VAPID_PRIVATE_KEY,
+            vapid_private_key="uKPy5ZdOSz5sF0yFqAxUyjZC5z4oZE83p7o3i4KNY1E",
             vapid_claims={"sub": settings.VAPID_CLAIMS},
         )
     except WebPushException as ex:
