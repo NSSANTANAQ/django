306db6cd5e85249ad0775b60ccef08808019ae8b
NSANTANAQ <nestorsantana12@gmail.com>
2024-12-23 12:36:50 -0500

    Implementación de apis

--- mysite/settings.py
+++ mysite/settings.py
@@ -240,24 +240,24 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,                # Invalidar tokens antiguos
 }
 
-# import os
-# from firebase_admin import credentials, initialize_app
-#
-#
-# # Configurar credenciales desde variables de entorno
-# cred = credentials.Certificate({
-#     "type": config("FIREBASE_TYPE"),
-#     "project_id": config("FIREBASE_PROJECT_ID"),
-#     "private_key_id": config("FIREBASE_PRIVATE_KEY_ID"),
-#     "private_key": config("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-#     "client_email": config("FIREBASE_CLIENT_EMAIL"),
-#     "client_id": config("FIREBASE_CLIENT_ID"),
-#     "auth_uri": config("FIREBASE_AUTH_URI"),
-#     "token_uri": config("FIREBASE_TOKEN_URI"),
-#     "auth_provider_x509_cert_url": config("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-#     "client_x509_cert_url": config("FIREBASE_CLIENT_CERT_URL")
-# })
-#
-#
-# if not firebase_admin._apps:  # Evitar múltiples inicializaciones
-#     firebase_admin.initialize_app(cred)
+import os
+from firebase_admin import credentials, initialize_app
+
+
+# Configurar credenciales desde variables de entorno
+cred = credentials.Certificate({
+    "type": config("FIREBASE_TYPE"),
+    "project_id": config("FIREBASE_PROJECT_ID"),
+    "private_key_id": config("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": config("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": config("FIREBASE_CLIENT_EMAIL"),
+    "client_id": config("FIREBASE_CLIENT_ID"),
+    "auth_uri": config("FIREBASE_AUTH_URI"),
+    "token_uri": config("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": config("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": config("FIREBASE_CLIENT_CERT_URL")
+})
+
+
+if not firebase_admin._apps:  # Evitar múltiples inicializaciones
+    firebase_admin.initialize_app(cred)
