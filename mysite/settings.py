47cdfed6b64cf305c09377aaeb1500b2539f3c7a
NSANTANAQ <nestorsantana12@gmail.com>
2025-01-17 00:57:56 -0500

    Implementación de apis

--- mysite/settings.py
+++ mysite/settings.py
@@ -227,7 +227,6 @@ VAPID_EMAIL = os.getenv('VAPID_EMAIL', "mailto:nestorsantana12@gmail.com")
 # Configuración de JWT
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
