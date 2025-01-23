88af12284e14b31ee3cdc805327fd2a63949cfb7
NSANTANAQ <nestorsantana12@gmail.com>
2025-01-23 12:58:47 -0500

    Implementación de apis

--- apis/serializer.py
+++ apis/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from cliente.models import AdCuenta
+from cliente.models import AdCuenta , RenLiquidacion
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,3 +14,8 @@ class CuentaSerializer(serializers.ModelSerializer):
     class Meta:
         model = AdCuenta
         fields = ['id', 'zona', 'sector', 'mz', 'direccion', 'nombre_calle', 'cliente', 'estado']
+
+class DetalleFinancieroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =RenLiquidacion
+        fields = ['mes_facturacion', 'saldo', 'estado_liquidacion']
